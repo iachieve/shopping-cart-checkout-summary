b35bcb7eb2d9df13451d09892e78a411af2a04f4
iachieve <mhalim80@hotmail.com>
2020-04-16 23:03:54 -0700

    Updates

---
+++ precache-manifest.ca0986c87783404f065dcb0875c83832.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c8392ff10e8da95066bcb6225560b57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e14107db99fb50239f0",
+    "url": "/static/css/main.0b3f3d9e.chunk.css"
+  },
+  {
+    "revision": "44eb1470196b33405bd6",
+    "url": "/static/js/2.1764988d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1764988d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e14107db99fb50239f0",
+    "url": "/static/js/main.2a1f615e.chunk.js"
+  },
+  {
+    "revision": "ccf533e1e0ae5963497d",
+    "url": "/static/js/runtime-main.56f7d143.js"
+  },
+  {
+    "revision": "804ce29bf98c7e479c2880a2af9373a6",
+    "url": "/static/media/chair.804ce29b.jpeg"
+  }
+]);
